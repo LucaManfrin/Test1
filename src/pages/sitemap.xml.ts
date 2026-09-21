97cc8603f1dba5e5b9f36d61acb600cc1e4a0d38
LucaManfrin <lucamanfrin32@gmail.com>
2026-09-21 22:53:55 +0200

    Update sitemap.xml.ts

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -3,40 +3,22 @@ import { SITE, CATEGORIES } from "../site.config";
 
 export async function GET() {
   const posts = await getAllPosts();
+  
+  const urls = [
+    `${SITE.url}`,
+    `${SITE.url}/posts`,
+    `${SITE.url}/search`,
+    ...CATEGORIES.map((cat) => `${SITE.url}/category/${cat.slug}`),
+    ...posts.map((post) => `${SITE.url}/posts/${post.slug}`),
+  ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${SITE.url}</loc>
-  </url>
-  <url>
-    <loc>${SITE.url}/posts</loc>
-  </url>
-  <url>
-    <loc>${SITE.url}/search</loc>
-    <priority>0.7</>
-  </url>
-  ${CATEGORIES.map(
-    (cat) => `
-  <url>
-    <loc>${SITE.url}/category/${cat.slug}</loc>
-    <priority>0.8</priority>
-  </url>`
-  ).join("")}
-  ${posts
-    .map(
-      (post) => `
-  <url>
-    <loc>${SITE.url}/posts/${post.slug}</loc>
-    <lastmod>${post.data.date.toISOString().split("T")[0]}</lastmod>
-  </url>`
-    )
-    .join("")}
+${urls.map((url) => `  <url><loc>${url}</loc></url>`).join("\n")}
 </urlset>`;
 
-  return new Response(sitemap, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
+  return new Response(sitemap.trim(), {
+    status: 200,
+    headers: { "Content-Type": "application/xml; charset=utf-8" },
   });
 }
