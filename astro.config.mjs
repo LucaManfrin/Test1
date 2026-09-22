5c4977f308ea3ba700074ff62729da847b77edd6
LucaManfrin <lucamanfrin32@gmail.com>
2026-09-22 23:19:43 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { SITE } from "./src/site.config.ts";
 export default defineConfig({
   output: "static",
   site: SITE.url,
-  trailingSlash: "never", 
+  trailingSlash: "never",
   build: {
     format: "directory",
   },
